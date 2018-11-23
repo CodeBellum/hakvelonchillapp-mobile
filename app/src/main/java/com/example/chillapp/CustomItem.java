56a5e1d485556345cb92e0a060642fce74d95e9e
Meowth <meowththatsright6@gmail.com>
2018-11-23 10:55:22 +0300

    Запилил бд

--- app/src/main/java/com/example/chillapp/CustomItem.java
+++ app/src/main/java/com/example/chillapp/CustomItem.java
@@ -4,10 +4,14 @@ public class CustomItem {
     public int id;
     public String primaryText;
     public String secondaryText;
+    public int firstTextSize;
+    public int secondTextSize;
 
-    public CustomItem(int id, String p, String s) {
+    public CustomItem(int id, String p, String s, int fs, int ss) {
         this.id = id;
         this.primaryText = p;
         this.secondaryText = s;
+        this.firstTextSize = fs;
+        this.secondTextSize = ss;
     }
 }
--- app/src/main/java/com/example/chillapp/MainActivity.java
+++ app/src/main/java/com/example/chillapp/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.chillapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DatabaseHelper dbHelper;
+    private Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        dbHelper = new DatabaseHelper(context);
     }
 }
