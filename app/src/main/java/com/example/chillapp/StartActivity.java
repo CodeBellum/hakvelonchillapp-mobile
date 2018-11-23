6ff162795af86fcc51954df45695d6935d7a0087
Meowth <meowththatsright6@gmail.com>
2018-11-23 17:21:15 +0300

    Доверстал стартовое активити

--- app/src/main/java/com/example/chillapp/StartActivity.java
+++ app/src/main/java/com/example/chillapp/StartActivity.java
@@ -1,10 +1,23 @@
 package com.example.chillapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button airplaneModeBtn, enterBtn;
+    private Context context = this;
+    private Timer timer;
+    private AnySecond anySecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +25,50 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        timer = new Timer();
+        anySecond = new AnySecond();
+        timer.scheduleAtFixedRate(anySecond,1000,2000);
+        airplaneModeBtn = (Button) findViewById(R.id.airplaneMode);
+        enterBtn = (Button) findViewById(R.id.enter);
+        if (isAirplaneMode()) airplaneModeBtn.setVisibility(View.GONE);
+        else airplaneModeBtn.setVisibility(View.VISIBLE);
+        airplaneModeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(
+                        new Intent(android.provider.Settings.ACTION_AIRPLANE_MODE_SETTINGS));
+            }
+        });
+        enterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.cancel();
+            }
+        });
+    }
+    public boolean isAirplaneMode() {
+        return Settings.System.getInt(this.getContentResolver(),
+                Settings.System.AIRPLANE_MODE_ON, 0) == 1;
+    }
+
+    @Override
+    protected void onResume() {
+        if (isAirplaneMode()) airplaneModeBtn.setVisibility(View.GONE);
+        else airplaneModeBtn.setVisibility(View.VISIBLE);
+        super.onResume();
+    }
+
+    class AnySecond extends TimerTask{
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (isAirplaneMode()) airplaneModeBtn.setVisibility(View.GONE);
+                    else airplaneModeBtn.setVisibility(View.VISIBLE);
+                }
+            });
+
+        }
     }
 }
